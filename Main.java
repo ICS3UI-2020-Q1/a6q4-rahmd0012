14a72798c67b8f7ad1a9765a3a06015e14060308
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-08 17:05:13 +0000

    Found out the dot product from user entered numbers

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program to figure out the dot product using 3d vectors
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +10,40 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    int[] integers = new int[3];
+        
+    // ask the user to enter 3 values
+    System.out.println("Please enter the 3 values for the first vector");
+    // create a for loop to get the 3 values
+    for(int i = 0; i < 3; i++){
+    integers[i] = input.nextInt();
+    }
+
+    int[] integers2 = new int[3];
+
+    // ask the user to enter 3 more values
+    System.out.println("Please enter the 3 values for the second vector");
+    // create a for loop to get the second 3 values
+    for(int i = 0; i < 3; i++){
+    integers2[i] = input.nextInt();
+    }
+
+    // create a variable that multiplies the x values
+    int xValue = (integers[0]) * (integers2[0]);
+
+    // create a variable that multiplies the y values
+    int yValue = (integers[1]) * (integers2[1]);
+
+    // create a variable that multiplies the z values
+    int zValue = (integers[2]) * (integers2[2]);
+
+    // create a variables that adds them all
+    int total = xValue + yValue + zValue;
+
+    // tell the user the dot product
+    System.out.println("The dot product is " + total);
   }
 }
